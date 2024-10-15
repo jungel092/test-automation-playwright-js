09b25c1c97b8f0a7c53d03cb5eddad8ccddd4dfb
jungel092 <jungel092@github.mail>
2024-10-15 22:47:02 +0200

    Implementace prvniho zadani

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -1,6 +1,12 @@
 import { test } from "@playwright/test";
 
 test("should open login page", async ({ page }) => {
-    await page.goto("/prihlaseni");
+    await page.goto("https://team8-2022brno.herokuapp.com/prihlaseni");
     console.log(await page.title());
+});
+
+test("should open registration page and take screenshot", async ({ page }) => {
+    await page.goto("https://team8-2022brno.herokuapp.com/registrace");
+
+    await page.screenshot({ path: "registration_page.png" });
 });
